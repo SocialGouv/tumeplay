4d3ef395aaff28581443402d2acf1845267475c7
Brian Ridolce <brianridolce@gmail.com>
2021-06-14 14:16:18 +0200

    adding security check for box availability

--- back-strapi/api/box/services/box.js
+++ back-strapi/api/box/services/box.js
@@ -5,4 +5,15 @@
  * to customize this service
  */
 
-module.exports = {};
+ module.exports = {
+  async decrement(box_id, quantity) {
+    await strapi.query('box').model.query((qb) => {
+      qb.where('id', box_id);
+      qb.increment('stock', - quantity);
+    }).fetch();
+  },
+  async checkBoxAvailability(box_id) {
+    const box = await strapi.services.box.findOne({ id: box_id })
+    return box && box.stock > 0;
+  },
+};
--- back-strapi/api/commande/controllers/commande.js
+++ back-strapi/api/commande/controllers/commande.js
@@ -14,8 +14,15 @@ module.exports = {
       ctx.request.body.content[0].produits.forEach((product_wrapper) => {
         strapi.services["box-sur-mesure"].decrement(product_wrapper.produit, product_wrapper.quantity)
       })
+    } else if (ctx.request.body.content[0].__component === 'commandes.box') {
+      const box_id = ctx.request.body.content[0].box
+      const available = await strapi.services.box.checkBoxAvailability(box_id)
+      if (available) {
+        strapi.services["box"].decrement(box_id, 1)
+      } else {
+        return ctx.badRequest(null, 'Box ' + box_id + ' unavailable');
+      }
     }
     return entity;
-  },
-
+  }
 };
