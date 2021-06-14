eb2851b138d67c50ad9290fb8d7fd10d91eeb987
Brian Ridolce <brianridolce@gmail.com>
2021-06-14 12:54:59 +0200

    custom request handler for stock decrementation

--- back-strapi/api/box-sur-mesure/services/box-sur-mesure.js
+++ back-strapi/api/box-sur-mesure/services/box-sur-mesure.js
@@ -5,4 +5,11 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  async decrement(product_id, quantity) {
+    await strapi.query('stocks.box-produit-sur-mesure').model.query((qb) => {
+      qb.where('produit', product_id);
+      qb.increment('stock', - quantity);
+    }).fetch();
+  }
+};
--- back-strapi/api/commande/controllers/commande.js
+++ back-strapi/api/commande/controllers/commande.js
@@ -1,8 +1,21 @@
 'use strict';
 
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    let entity;
+      entity = await strapi.services.commande.create(ctx.request.body);
+    if (ctx.request.body.content[0].__component === 'commandes.box-sur-mesure') {
+      ctx.request.body.content[0].produits.forEach((product_wrapper) => {
+        strapi.services["box-sur-mesure"].decrement(product_wrapper.produit, product_wrapper.quantity)
+      })
+    }
+    return entity;
+  },
+
+};
