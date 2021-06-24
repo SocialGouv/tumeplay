824db2e5ebdf407fcc0d12d489e318105f3ef393
Clément <clement@numericite.eu>
2021-06-24 18:39:18 +0200

    fix:uncomment data-transfer functions

--- data-transfer/main.py
+++ data-transfer/main.py
@@ -4,13 +4,13 @@ tumeplay_base_url = "https://tumeplay-api.fabrique.social.gouv.fr"
 tumeplay_api = tumeplay_base_url + "/api"
 strapi_base_api = "http://localhost:1337"
 
-# # CONTENTS
-# theme_ids = themes.import_themes(tumeplay_base_url, tumeplay_api, strapi_base_api)
-# contents.import_contents(tumeplay_base_url, tumeplay_api, strapi_base_api, theme_ids)
-# questions.import_questions(tumeplay_base_url, tumeplay_api, strapi_base_api, theme_ids)
-#
+# CONTENTS
+theme_ids = themes.import_themes(tumeplay_base_url, tumeplay_api, strapi_base_api)
+contents.import_contents(tumeplay_base_url, tumeplay_api, strapi_base_api, theme_ids)
+questions.import_questions(tumeplay_base_url, tumeplay_api, strapi_base_api, theme_ids)
+
 # STOCKS
 box_ids = boxes.import_boxes(tumeplay_base_url, tumeplay_api, strapi_base_api)
 
 # COMMANDES
-# orders.import_orders(tumeplay_api, strapi_base_api, box_ids)
+orders.import_orders(tumeplay_api, strapi_base_api, box_ids)
