dcd2ab4b938222d5cb9717180b6832e6c73f4237
Clément <clement@numericite.eu>
2021-06-30 14:21:16 +0200

    feat: send email with colissimo csv at 7AM

--- back-strapi/config/functions/cron.js
+++ back-strapi/config/functions/cron.js
@@ -10,12 +10,64 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#cron-tasks
  */
 
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const _ = require('lodash');
+const fs = require('fs');
+
+const EMAIL_DAILY_ORDERS = {
+  subject: 'Les commandes du jour',
+  text: `
+    Bonjour,\n
+    Vous trouverez en pièce jointe les commandes du <%= date %>.\n
+    Cordialement,\n
+    Le back-office Tumeplay.
+  `,
+  html: `
+    <p>Bonjour,</p>
+    <p>Vous trouverez en pièce jointe les commandes du <%= date %>.</p>
+    <p>
+      Cordialement,<br>
+      Le back-office Tumeplay.
+    </p>
+  `
+}
+
+const createColissimoCsv = async (dirpath, today_7AM, orders) => {
+  const csvColissimoPath = dirpath + 'orders_colissimo_' + today_7AM.getTime() + '.csv'
+  const csvColissimoWriter = createCsvWriter({
+    path: csvColissimoPath,
+    header: [
+      {id: 'id', title: 'ID'}, 
+      {id: 'last_name', title: 'Nom'}, 
+      {id: 'first_name', title: 'Prénom'}, 
+      {id: 'address', title: 'Adresse'}, 
+      {id: 'address_more', title: 'Complément d\'adresse'}, 
+      {id: 'address_zipcode', title: 'Code postale'}, 
+      {id: 'address_city', title: 'Commune'}, 
+      {id: 'phone', title: 'Téléphone'}
+    ]
+  });
+
+  let csvColissimoOrders = []
+  orders.forEach((order) => {
+    csvColissimoOrders.push(
+      _.pick(order, ['id', 'last_name', 'first_name', 'address', 'address_more', 'address_zipcode', 'address_city', 'phone'])
+    )
+  })
+
+  await csvColissimoWriter.writeRecords(csvColissimoOrders)
+
+  strapi.log.info('CSV COLISSIMO GENERATED WITH PATH : ', csvColissimoPath)
+
+  return csvColissimoPath
+};
+
 module.exports = {
   /**
    * Send email of daily orders
    * Every day at 7am.
    */
-  '* * * * *': async () => {
+  '0 7 * * *': async () => {
     const environnements = await strapi.services.environnement.find()
     const environnement_metropole = environnements.find(_ => _.slug === 'metropole')
     const environnement_guyane = environnements.find(_ => _.slug === 'guyane')
@@ -27,17 +79,31 @@ module.exports = {
     yesterday_7AM.setMilliseconds(0);
 
     let today_7AM  = new Date();
-    today_7AM.setHours(11);
+    today_7AM.setHours(18);
     today_7AM.setMinutes(0);
     today_7AM.setMilliseconds(0);
 
-    strapi.log.info('', yesterday_7AM)
-    strapi.log.info('', today_7AM)
+    const yesterDayFormatted = String(yesterday_7AM.getDate()).padStart(2, '0') + '/' + String(yesterday_7AM.getMonth() + 1).padStart(2, '0') + '/' + yesterday_7AM.getFullYear()
 
-    const orders = await strapi.services.commande.find({created_at_gte: yesterday_7AM.getTime(), created_at_lt: today_7AM.getTime(), _sort: 'created_date:desc'})
+    const ordersColissimo = await strapi.services.commande.find({created_at_gte: yesterday_7AM.getTime(), created_at_lt: today_7AM.getTime(), delivery: 'home', _sort: 'created_at:desc'})
 
-    const orders_metropole = 'toto'
+    const dirpath = 'public/uploads/orders/'
+    await fs.mkdirSync(dirpath, { recursive: true })
 
-    strapi.log.debug('', orders[0].box)
+    const csvColissimoPath = await createColissimoCsv(dirpath, today_7AM, ordersColissimo)
+
+    await strapi.plugins['email'].services.email.sendTemplatedEmail(
+      {
+        to: strapi.config.get('server.emails.cat'),
+        cc: strapi.config.get('server.emails.admin'),
+        attachments: [
+          {filename: 'commandes_colissimo_' + yesterDayFormatted.split('/').join('') + '.csv', content: fs.createReadStream(csvColissimoPath)},
+        ]
+      },
+      EMAIL_DAILY_ORDERS,
+      {
+        date: yesterDayFormatted
+      }
+    )
   }
 };
