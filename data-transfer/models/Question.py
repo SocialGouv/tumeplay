cd948c148da3e1ff40858b8a485fc3174f078c7b
Clément <clement@numericite.eu>
2021-07-06 15:45:10 +0200

    feat: add sound to theme & data-transfer old thematiques sounds

--- data-transfer/models/Question.py
+++ data-transfer/models/Question.py
@@ -8,7 +8,7 @@ import os
 
 class Question:
 
-    def __init__(self, text_question, text_answer, image_url, theme_id, response_A, response_B, response_C, response_A_neutral, response_B_neutral, response_C_neutral, right_answer):
+    def __init__(self, text_question, text_answer, image_url, theme_id, response_A, response_B, response_C, response_A_neutral, response_B_neutral, response_C_neutral, right_answer, sound_url):
         self.text_question = text_question.strip()
         self.text_answer = text_answer.strip()
         self.image_url = image_url.replace(" ", "%20").replace("&amp;", "&")
@@ -20,6 +20,7 @@ class Question:
         self.response_B_neutral = response_B_neutral
         self.response_C_neutral = response_C_neutral
         self.right_answer = right_answer.strip()
+        self.sound_url = sound_url.replace(" ", "%20").replace("&amp;", "&") if sound_url else None
 
     def get_image(self):
         try:
@@ -44,4 +45,35 @@ class Question:
             else:
                 return open(self.image_url, 'rb')
         except FileNotFoundError:
+            return None
+
+    def get_sound(self):
+        if self.sound_url is not None:
+            try:
+                pattern_url = re.compile(
+                    r'^(?:http|ftp)s?://'  # http:// or https://
+                    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+                    r'localhost|'  # localhost...
+                    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+                    r'(?::\d+)?'  # optional port
+                    r'(?:/?|[/?]\S+)$', re.IGNORECASE
+                )
+                if re.match(pattern_url, self.sound_url) is not None:
+                    try:
+                        file_url, file_extension = os.path.splitext(self.sound_url)
+                        response = urllib.request.urlopen(self.sound_url)
+                        tmp_file = tempfile.NamedTemporaryFile(delete=False)
+                        shutil.copyfileobj(response, tmp_file)
+                        os.rename(tmp_file.name, tmp_file.name + (file_extension if file_extension != '' else '.mp3'))
+                        return open(tmp_file.name + (file_extension if file_extension != '' else '.mp3'), 'rb')
+                    except (HTTPError, URLError, UnicodeEncodeError) as e:
+                        print('ERROR : ', e)
+                        return None
+                else:
+                    print('NOT URL : ', self.sound_url)
+                    return open(self.sound_url, 'rb')
+            except FileNotFoundError:
+                print('FILE NOT FOUND')
+                return None
+        else:
             return None
