f3bd8001d0d6171860bc19277f5da0a1b5aee960
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-07-26 13:47:20 +0200

    fix(k8s): fix tag

--- .k8s/components/backend.ts
+++ .k8s/components/backend.ts
@@ -15,7 +15,7 @@ import {
 const getGithubRef = (env: Record<string, any>) => {
   const ref =
     env.GITHUB_REF && env.GITHUB_REF.startsWith("refs/tags/")
-      ? env.GITHUB_REF.split("/").pop()
+      ? env.GITHUB_REF.split("/").pop().replace(/^v/, "")
       : `sha-${env.GITHUB_SHA}`;
   return ref;
 };
