0ac712c82ce8051017ba06048c7ad586a20cb1ff
Clément <clement@numericite.eu>
2021-08-31 09:50:12 +0200

    fix: cron handle GTM hour

--- back-strapi/config/functions/cron.js
+++ back-strapi/config/functions/cron.js
@@ -173,19 +173,19 @@ module.exports = {
    * Send email of daily orders
    * Every day at 7am.
    */
-  '0 7 * * *': async () => {
+  '0 5 * * *': async () => {
     const environnements = await strapi.services.environnement.find()
     const environnement_metropole = environnements.find(_ => _.slug === 'metropole')
     const environnement_guyane = environnements.find(_ => _.slug === 'guyane')
 
     let yesterday_7AM  = new Date();
     yesterday_7AM.setDate(yesterday_7AM.getDate() - 1);
-    yesterday_7AM.setHours(7);
+    yesterday_7AM.setHours(5);
     yesterday_7AM.setMinutes(0);
     yesterday_7AM.setMilliseconds(0);
 
     let today_7AM  = new Date();
-    today_7AM.setHours(7);
+    today_7AM.setHours(5);
     today_7AM.setMinutes(0);
     today_7AM.setMilliseconds(0);
 
