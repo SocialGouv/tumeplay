9f44753e1a692b3828e6bc1c5a79d58a9f529df8
Brian Ridolce <brianridolce@gmail.com>
2021-08-31 11:14:07 +0200

    feat/ Adjusting delivery type display

--- tumeplay-dashboard/src/components/ui/TableRow.jsx
+++ tumeplay-dashboard/src/components/ui/TableRow.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import chronoLogo from "../../assets/pictures/Apps-Colissimo.jpeg"
+import mondialRelay from "../../assets/pictures/mondial-relay.jpeg"
 
 const TableRow = (props) => {
-  const { items, numberPerPage } = props;
+  const { items } = props;
 
   const linesToDisplay = items.map((item) => {
     item.printed = false;
+    item.selected = false;
     return(
       <tr key={item.id} className="text-center">
         <td>
-          <input type='checkbox' />
+          <input type='checkbox' value={item.isSelected} />
         </td>
         <td className="tmp-table-td">{item.id}</td>
         <td className="tmp-table-td">{new Date(item.created_at).toLocaleDateString()}</td>
-        <td className="tmp-table-td">{item.delivery === 'home' ? "Colissimo" : "Mondial Relay"}</td>
+        <td className="tmp-table-td">
+          <div className="flex align-middle justify-around">
+            <img className="w-10 h-10" src={item.delivery === 'home' ? chronoLogo : mondialRelay} alt="logo transporteur" />
+            <p className="my-auto">{item.delivery === 'home' ? "Colissimo" : "Mondial Relay"}</p>
+          </div>
+          </td>
         <td className="tmp-table-td">{item.printed ? <FontAwesomeIcon icon={faCheck} color="green" /> : <FontAwesomeIcon icon={faTimes} color='red' /> }</td>
         <td className="tmp-table-td">{item.printed ? <FontAwesomeIcon icon={faCheck} color="green" /> : <FontAwesomeIcon icon={faTimes} color='red' /> }</td>
       </tr>
