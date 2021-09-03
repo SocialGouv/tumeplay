963e127ceceb118ca1508262e8a786c9eadfa963
Clément <clement@numericite.eu>
2021-09-03 16:26:27 +0200

    fix: login page design

--- tumeplay-dashboard/src/views/auth/Login.jsx
+++ tumeplay-dashboard/src/views/auth/Login.jsx
@@ -28,49 +28,95 @@ const Login = () => {
     }
   }
 
-  return(
-    <div className="mx-auto px-4 w-screen h-screen">
-      <div className="flex content-center items-center justify-center align-middle h-full">
-        <div className="w-full lg:w-5/12 my-auto px-4">
-          <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
-            <div className="rounded-t mb-0 px-6 py-6">
-              <div className="text-center content-center mb-3">
-                <img className="w-auto h-auto object-cover mx-auto" src={logo} alt="" />
-                <h1 className="text-blueGray-500 text-4.5xl font-bold" >Connexion</h1>
-                <form onSubmit={() => {handleLogin()}} >
-                  <div className="mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    >
-                      Email
-                    </label>
-                    <input type="text" value={info.identifier || ''} onChange={e => handleChange(e)} name='identifier' className="relative px-2 py-1 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow outline-none w-1/2 pr-10 focus:outline-none focus:ring ease-linear transition-all duration-150" />
-                    <span className="z-10 leading-snug font-normal absolute text-center text-blueGray-300 bg-transparent rounded text-base items-center justify-center w-8 pr-4 py-1">
-                      <FontAwesomeIcon icon={faUser}/>
-                    </span>
-                  </div>
-                  <div className="mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    >
-                      Mot de passe
-                    </label>
-                    <div className="relative">
-                      <input type="password" value={info.password || ''} onChange={e => handleChange(e)} name='password' className="z-10 px-2 py-1 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow outline-none w-1/2 pr-10 focus:outline-none focus:ring ease-linear transition-all duration-150" />
-                      <span className="z-20 leading-snug font-normal absolute text-center text-blueGray-300 bg-transparent rounded text-base items-center justify-center w-8 pr-4 py-1">
-                        <FontAwesomeIcon icon={faLock}/>
-                      </span>
-                    </div>
-                  </div>
-                  <button onClick={() => {handleLogin()}} type="button" className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">Se connecter</button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+  return (
+    <>
+			<div className="flex content-center items-center justify-center h-screen">
+				<div className="w-full m-auto lg:w-4/12 px-4">
+					<div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+						<div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+							<div className="rounded-t mb-0 px-6 py-6">
+								<div className="text-center mb-4 mt-2">
+									<img src={logo} className="w-64 mx-auto"/>
+								</div>
+								<div className="text-center mb-3">
+									<h6 className="text-blueGray-500 text-sm font-bold">
+										Espace de gestion de la plateforme
+									</h6>
+								</div>
+								<hr className="mt-6 border-b-1 border-blueGray-300" />
+							</div>
+							<form onSubmit={(e) => {
+								e.preventDefault();
+								handleLogin()
+							}}>
+								<div className="relative w-full mb-3">
+									<label
+										className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+										htmlFor="grid-password"
+									>
+										Email
+									</label>
+									<input
+										type="email"
+										value={info.identifier || ''} onChange={e => handleChange(e)} name="identifier"
+										className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+										placeholder="Email"
+									/>
+								</div>
+
+								<div className="relative w-full mb-3">
+									<label
+										className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+										htmlFor="grid-password"
+									>
+										Mot de passe
+									</label>
+									<input
+										type="password"
+										value={info.password || ''} onChange={e => handleChange(e)} name="password"
+										className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+										placeholder="Mot de passe"
+									/>
+								</div>
+								<div>
+									<label className="inline-flex items-center cursor-pointer">
+										<input
+											id="customCheckLogin"
+											type="checkbox"
+											className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+										/>
+										<span className="ml-2 text-sm font-semibold text-blueGray-600">
+											Se souvenir de moi
+										</span>
+									</label>
+								</div>
+
+								<div className="text-center mt-6">
+									<button
+										className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+										type="submit"
+									>
+										Se connecter
+									</button>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div className="flex flex-wrap mt-6 relative">
+						<div className="w-1/2">
+							<a
+								href="#pablo"
+								onClick={(e) => e.preventDefault()}
+								className="text-blueGray-200"
+							>
+								<small>Mot de passe oublié ?</small>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+    </>
+  );
 }
 
 export default Login;
