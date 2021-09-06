f09be9131d44234d195af12aa4b00616f8aefac0
Clément <clement@numericite.eu>
2021-09-06 16:10:48 +0200

    feat: reset password

--- tumeplay-dashboard/src/views/auth/ResetPassword.jsx
+++ tumeplay-dashboard/src/views/auth/ResetPassword.jsx
@@ -1,18 +1,28 @@
 import React, {useContext, useState} from 'react'
 import AppContext from '../../AppContext';
-import { forgotPassword } from '../../lib/auth';
+import { resetPassword } from '../../lib/auth';
 import logo from '../../assets/pictures/full-logo.png';
 import checkIcon from '../../assets/pictures/check-icon.png';
 import Loader from '../../components/ui/Loader';
 import { Formik } from 'formik';
 import FormErrorMessage from '../../components/ui/FormErrorMessage';
 import ErrorMessage from '../../components/ui/ErrorMessage';
+import {
+	Link,
+  useLocation
+} from "react-router-dom";
 
+// A custom hook that builds on useLocation to parse
+// the query string for you.
+function useQuery() {
+	return new URLSearchParams(useLocation().search);
+}
 
-const ForgotPassword = () => {
-
+const ResetPassword = () => {
 	const [serverError, setServerError] = useState(null)
-	const [mailSent, setMailSent] = useState(false)
+	const [resetDone, setResetDone] = useState(false)
+
+	let query = useQuery();
 
   return (
     <>
@@ -26,43 +36,62 @@ const ForgotPassword = () => {
 								</div>
 								<div className="text-center mb-3">
 									<h6 className="text-blueGray-500 text-sm font-bold">
-										Demande de réinitialisation de mot de passe
+										Réinitialisation de votre mot de passe
 									</h6>
 								</div>
 								<hr className="mt-6 border-b-1 border-blueGray-300" />
 							</div>
 							{
-								mailSent 
+								resetDone 
 								?
 								<div className="flex flex-col items-center justify-center">
 									<img src={checkIcon} className="w-12" />
-									<p className="text-blueGray-500 px-12 text-center mt-4">Un email contenant un lien pour réinitialiser votre mot de passe vient de vous être envoyé.</p>
+									<p className="text-blueGray-500 px-12 text-center mt-4">
+										Votre mot de passe a bien été modifié.
+									</p>
+									<Link
+										to="/"
+										className="bg-blueGray-800 mt-4 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+									>
+										Retour à l'écran de connexion
+									</Link>
 								</div>
 								:
 								<Formik
-								initialValues={{email: '', password: ''}}
+								initialValues={{password: '', password2: ''}}
 								validate={values => {
 									setServerError(null);
 									const errors = {};
-									if (!values.email) {
-										errors.email = 'Ce champ est requis';
+									if (!values.password) {
+										errors.password = 'Ce champ est requis';
 									} else if (
-										!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+										!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i.test(values.password)
 									) {
-										errors.email = 'Adresse email incorrecte';
+										errors.password = 'Le mot de passe doit comporter au moins 8 caractères et doit comporter au moins un caractère numérique.'
+									}
+									
+									if (!values.password2) {
+										errors.password2 = 'Ce champ est requis';
+									} 
+									
+									if (values.password && values.password2 && values.password !== values.password2) {
+										errors.password2 = 'Les deux mot de passe ne correspondent pas';
 									}
 
 									return errors;
 								}}
 								onSubmit={async (values, { setSubmitting }) => {
-									forgotPassword(values.email)
+									resetPassword(
+										values.password,
+										values.password2,
+										query.get('code')
+									)
 									.then(() => {
-										setMailSent(true)
+										setResetDone(true)
 										setSubmitting(false);
 									}, (res) => {
 										if (res.response.status === 400) {
-											console.log('here')
-											setServerError('Cet email n\'existe pas dans notre base de donnée')
+											setServerError('Votre lien est invalide')
 										} else {
 											setServerError('Désolé, une erreur s\'est produite, veuillez réessayer ultérieurement')
 										}
@@ -84,14 +113,30 @@ const ForgotPassword = () => {
 												className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
 												htmlFor="grid-password"
 											>
-												Email
+												Nouveau mot de passe
 											</label>
 											<input
-												type="email"
-												value={values.email} onChange={handleChange} onBlur={handleBlur} name="email"
+												type="password"
+												value={values.password} onChange={handleChange} onBlur={handleBlur} name="password"
 												className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-												placeholder="Email"
+												placeholder="Taper votre nouveau mot de passe..."
 											/>
+											<FormErrorMessage errors={errors} touched={touched} name="password" />
+										</div>
+										<div className="relative w-full mb-3">
+											<label
+												className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+												htmlFor="grid-password"
+											>
+												Répetez le nouveau mot de passe
+											</label>
+											<input
+												type="password"
+												value={values.password2} onChange={handleChange} onBlur={handleBlur} name="password2"
+												className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+												placeholder="Retapez le mot de passe..."
+											/>
+											<FormErrorMessage errors={errors} touched={touched} name="password2" />
 										</div>
 										<FormErrorMessage errors={errors} touched={touched} name="email" />
 										<div className="text-center mt-6">
@@ -109,7 +154,7 @@ const ForgotPassword = () => {
 													className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
 													type="submit"
 												>
-													Valider ma demande
+													Valider le mot de passe
 												</button>
 											}
 											{
@@ -130,4 +175,4 @@ const ForgotPassword = () => {
   );
 }
 
-export default ForgotPassword;
+export default ResetPassword;
