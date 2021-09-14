3a8f363d3a286006ca7a73d924a40e4537a5ffb7
Brian Ridolce <brianridolce@gmail.com>
2021-09-14 18:03:01 +0200

    feat: revamping table component for reusable purpose + Orders Summary componnent initiation + react datePicker

--- tumeplay-dashboard/src/components/Table.jsx
+++ tumeplay-dashboard/src/components/Table.jsx
@@ -1,15 +1,55 @@
-import React, {useEffect, useState} from 'react';
-import TableRow from './ui/TableRow';
+import React from 'react';
+import { faPaperPlane, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import chronoLogo from "../assets/pictures/Apps-Colissimo.jpeg"
+import mondialRelay from "../assets/pictures/mondial-relay.jpeg"
 
-const Table = ({items, titles, numberPerPage, handleSpecificSelection, handleSelectAll}) => {
+const Table = ({dataToDisplay, numberPerPage, handleSpecificSelection, handleSelectAll}) => {
 
-  const titlesToDisplay = titles.map((title,index) => {
+  const titlesToDisplay = dataToDisplay.headers.map((item,index) => {
     return(
       <th key={index} className="tmp-table-th">
-        {title}
+        {item.name}
       </th>
     )
   })
+
+  const formatDisplay = (item, fieldName) => {
+    if (fieldName === "created_at" || fieldName === "date_sent") {
+      item = new Date(item).toLocaleDateString()
+    }
+    if (fieldName === "delivery") {
+      return(
+        <div className="flex align-middle justify-around">
+          <img className="w-10 h-10" src={item === 'home' ? chronoLogo : mondialRelay} alt="logo transporteur" />
+          <p className="my-auto">{item === 'home' ? "Colissimo" : "Mondial Relay"}</p>
+        </div>
+      )
+    }
+    if (fieldName === "sent") {
+      return(
+        <>{item ? <FontAwesomeIcon icon={faPaperPlane} color="green" /> : <FontAwesomeIcon icon={faTimes} color='red' /> }</>
+      )
+    }
+    return item
+  }
+
+  const Rows = dataToDisplay.items.map((item) => {
+    return(
+      <tr className="text-center">
+        <td>
+         <input type='checkbox' id={item.id} value={item.selected} checked={item.selected} onChange={(e) => {handleSpecificSelection(e)}} />
+      </td>
+        {dataToDisplay.headers.map(h => {
+          return(
+            <td className="tmp-table-td">
+              {formatDisplay(item[h.fieldName], h.fieldName)}
+            </td>  )
+        })}
+      </tr>
+    )
+  })
+
 
   return (
      <div className="tmp-table-container shadow-md rounded-xl">
@@ -23,7 +63,7 @@ const Table = ({items, titles, numberPerPage, handleSpecificSelection, handleSel
               </tr>
             </thead>
             <tbody>
-              <TableRow items={items} numberPerPage={numberPerPage} handleSpecificSelection={handleSpecificSelection} />
+              {Rows}
             </tbody>
           </table>
       </div>
