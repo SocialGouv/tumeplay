935c4340f82b839f964663d235a28ef30f59c05b
Brian Ridolce <brianridolce@gmail.com>
2021-09-16 10:37:50 +0200

    feat: CSV FileName + Headers // Box number display

--- tumeplay-dashboard/src/views/admin/Bilan.jsx
+++ tumeplay-dashboard/src/views/admin/Bilan.jsx
@@ -4,8 +4,6 @@ import Table from '../../components/Table';
 import OrdersAPI from '../../services/api/orders';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
-import Pagination from "react-pagination-js";
-import Dropdown from "react-dropdown";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFileExcel } from '@fortawesome/free-solid-svg-icons';
 import { CSVLink } from "react-csv";
@@ -19,12 +17,8 @@ const Bilan = () => {
   // 4. Bouton pour générer un CSV ou PDF à la volée
 
   const [dailyOrders, setDailyOrders] = useState([])
-  const [numberPerPage,setNumberPerPage] = useState(5)
   const [tmpSelected, setTmpSelected] = useState([])
   const [currentDate, setCurrentDate] = useState(new Date())
-  const [pageItems, setPageItems] = useState([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const dropdownOptions = ['5', '10', '50', '100', {value: dailyOrders.length, label: 'Tout'}]
 
 
   const {token} = useContext(AppContext)
@@ -34,6 +28,7 @@ const Bilan = () => {
     let tmpOrders = res.data;
     tmpOrders.map(o => {
       o.selected = false
+      o.box_num = o.content[0].box.number
     })
     setDailyOrders([...tmpOrders])
   }
@@ -65,35 +60,31 @@ const Bilan = () => {
     }
   }
 
-  const onPageChange = (event) => {
-    setCurrentPage(event)
-  }
-
-  const handleChangeNumPerPage = (e) => {
-    setTmpSelected([])
-    setNumberPerPage(parseInt(e.value))
-  }
-
-  useEffect(() => {
-    const offset = (currentPage - 1) * numberPerPage;
-    let tmpFiltered = dailyOrders.slice(offset, offset + numberPerPage)
-    console.log('tmpFiltered', tmpFiltered)
-    setPageItems([...tmpFiltered])
-    setTmpSelected([])
-  }, [dailyOrders, currentPage, numberPerPage])
-
-  const boxpath = 'content[0]'
-
   const dataToDisplay = {
     headers: [
      {name: "Date", fieldName: 'date_sent'},
      {name: "Id", fieldName: 'id' },
-     {name: "Box", fieldName: boxpath},
+     {name: "Box", fieldName: "box_num"},
      {name: "Transporteur", fieldName: 'delivery'}
     ],
-    items: pageItems
+    items: dailyOrders
   }
 
+  const csvHeaders = [
+    {label: "Id", key: 'id'},
+    {label: "Date de traitement", key: 'date_sent'},
+    {label: "Numéro de boite", key: 'box_num'},
+    {label: "Transporteur", key: 'delivery'},
+  ]
+
+  const csvData = tmpSelected.map(o => {
+    let tmpOrder = {};
+    tmpOrder['id'] = o.id;
+    tmpOrder['date_sent'] = o.date_sent;
+    tmpOrder['box_num'] = o.content[0].box.number;
+    tmpOrder['delivery'] = o.delivery === "home" ? "Colissimo" : "Mondial Relay";
+    return tmpOrder
+  })
 
   return(
     <div className="container mt-10 mx-auto relative">
@@ -105,25 +96,15 @@ const Bilan = () => {
                     showPopperArrow={false}
         />
       </div>
-       <div className="tmp-dropdown-container" >
-        <Dropdown className='tmp-dropdown' menuClassName="tmp-dropdown-menu" options={dropdownOptions} onChange={(e) => handleChangeNumPerPage(e)} value={numberPerPage.toString()} />
-      </div>
       <Table dataToDisplay={dataToDisplay}
              handleSpecificSelection={handleSpecificSelection}
              handleSelectAll={handleSelectAll}
        />
-       <div className="tmp-pagination-container">
-        <Pagination
-          currentPage={currentPage}
-          totalSize={dailyOrders.length}
-          sizePerPage={numberPerPage}
-          numberOfPagesNextToActivePage={3}
-          changeCurrentPage={(event) => onPageChange(event)}
-          theme="border-bottom"
-        />
-      </div>
       {tmpSelected.length > 0 ?
-        <CSVLink data={tmpSelected} className="w-1/5 my-5 flex justify-self-center justify-around bg-lightBlue-700 p-2 rounded-md shadow-md">
+        <CSVLink data={csvData}
+                 headers={csvHeaders}
+                 filename={`Export-${new Date().toLocaleDateString()}`}
+                 className="tmp-csv-button">
           <FontAwesomeIcon icon={faFileExcel} color='white' size="lg" className='mr-2 mt-1'/>
           <p className="text-white my-1">Exporter en CSV</p>
         </CSVLink>
