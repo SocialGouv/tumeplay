e24d00dd7c704a39acca0e529d80d3c271adfd9b
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-09-23 11:39:38 +0200

    Change dashboard url

--- .k8s/components/dashboard.ts
+++ .k8s/components/dashboard.ts
@@ -3,9 +3,9 @@ import { create } from "@socialgouv/kosko-charts/components/nginx";
 import environments from "@socialgouv/kosko-charts/environments";
 
 export default async () => {
-  const subdomain = "dashboard-tumeplay";
+  const subdomain = "bo-tumeplay";
   const ciEnv = environments(process.env);
-  const subDomainPrefix = (!ciEnv.isProduction && `dashboard-`) || undefined;
+  const subDomainPrefix = (!ciEnv.isProduction && `bo-`) || undefined;
   const image = `harbor.fabrique.social.gouv.fr/tumeplay/dashboard:${ciEnv.tag || ciEnv.sha}`;
 
   const manifests = await create("dashboard", {
