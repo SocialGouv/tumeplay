1c06b8eedfca3d85a43ad0b0b29654d6948201f3
BrianRid <51997673+BrianRid@users.noreply.github.com>
2021-09-29 15:26:43 +0200

    fix(dashboard): handling selector in table for multiselect (#147)

--- tumeplay-dashboard/src/views/admin/OrdersLogistics.jsx
+++ tumeplay-dashboard/src/views/admin/OrdersLogistics.jsx
@@ -20,6 +20,7 @@ const OrdersLogistics = () => {
 
   const context = useContext(AppContext)
   const token = context.token
+  const [checked, setChecked] = useState(false)
   const [viewAll, setViewAll] = useState(false)
   const [boxes, setBoxes] = useState([])
   const [orders, setOrders] = useState([])
@@ -117,15 +118,18 @@ const OrdersLogistics = () => {
     event.preventDefault()
     setOpenTab(box_number)
     retrieveOrders({sent_ne: true, box_number})
+    setChecked(false)
     history.push(`/orders/box/${box_number}`)
   }
 
   const handleSelectAll = (e) => {
     if(e.target.checked) {
+      setChecked(e.target.checked)
       orders.forEach(order => order.selected = e.target.checked)
       setTmpSelectedItems([...orders])
     } else {
-       orders.forEach(order => order.selected = e.target.checked)
+      setChecked(false)
+      orders.forEach(order => order.selected = e.target.checked)
       setTmpSelectedItems([])
     }
   }
@@ -380,6 +384,7 @@ const OrdersLogistics = () => {
 				</div>
 			</div>
       <Table  dataToDisplay={dataToDisplay}
+              checked={checked}
               handleSpecificSelection={handleSpecificSelection}
               handleSelectAll={handleSelectAll}
 							title={getCurentBoxTitle()}  />
