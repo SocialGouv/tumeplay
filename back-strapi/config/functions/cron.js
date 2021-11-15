6d921383014e9a202dc4d2f2add345c4396979b5
Brian Ridolce <brianridolce@gmail.com>
2021-11-15 16:24:28 +0100

    feat: mailing CAT

--- back-strapi/config/functions/cron.js
+++ back-strapi/config/functions/cron.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Cron config that gives you an opportunity
@@ -10,12 +10,12 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#cron-tasks
  */
 
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const _ = require('lodash');
-const fs = require('fs');
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const _ = require("lodash");
+const fs = require("fs");
 
 const EMAIL_DAILY_ORDERS = {
-  subject: 'Les commandes du jour',
+  subject: "Les commandes du jour",
   text: `
     Bonjour,\n
     Vous trouverez en pièce jointe les commandes du <%= yesterday_date %> 07h au <%= today_date %> 07h.\n
@@ -29,143 +29,190 @@ const EMAIL_DAILY_ORDERS = {
       Cordialement,<br>
       Le back-office Tumeplay.
     </p>
-  `
-}
+  `,
+};
 
 const getDateFormatted = (date) => {
-  const cleanISODate = new Date(date).toISOString().replace(/T/, ' ').replace(/\..+/, '');
-  const onlyDate = _.get(cleanISODate.split(' '), '0', '')
-  const onlyTime = _.get(cleanISODate.split(' '), '1', '')
-  const splittedOnlyDate = onlyDate.split('-')
-  const frenchDate = _.get(splittedOnlyDate, '2', '') + '/' + _.get(splittedOnlyDate, '1', '') + '/' + _.get(splittedOnlyDate, '0', '')
+  const cleanISODate = new Date(date)
+    .toISOString()
+    .replace(/T/, " ")
+    .replace(/\..+/, "");
+  const onlyDate = _.get(cleanISODate.split(" "), "0", "");
+  const onlyTime = _.get(cleanISODate.split(" "), "1", "");
+  const splittedOnlyDate = onlyDate.split("-");
+  const frenchDate =
+    _.get(splittedOnlyDate, "2", "") +
+    "/" +
+    _.get(splittedOnlyDate, "1", "") +
+    "/" +
+    _.get(splittedOnlyDate, "0", "");
 
-  return frenchDate + ' ' + onlyTime
-}
+  return frenchDate + " " + onlyTime;
+};
 
-const createColissimoCsv = async (dirpath, today_7AM, orders, environnement_id) => {
-
+const createColissimoCsv = async (
+  dirpath,
+  today_7AM,
+  orders,
+  environnement_id
+) => {
   const box_numbers = _.uniq(
     orders.map((order) => {
-      var box_number = _.get(order.content, '[0].box.number', null)
-      if (box_number) return box_number
+      var box_number = _.get(order.content, "[0].box.number", null);
+      if (box_number) return box_number;
     })
-  )
+  );
 
-  let csvColissimoOrders = {}
+  let csvColissimoOrders = {};
 
-  box_numbers.forEach((number) => csvColissimoOrders[number] = [])
+  box_numbers.forEach((number) => (csvColissimoOrders[number] = []));
 
   orders.forEach((order) => {
-    const box = _.get(order, 'content[0].box', {})
+    const box = _.get(order, "content[0].box", {});
     if (box.environnement === environnement_id) {
-      const date = getDateFormatted(_.get(order, 'created_at', ''))
-      csvColissimoOrders[box.number].push(
-        {
-          ..._.pick(order, ['id', 'last_name', 'first_name', 'address', 'address_more', 'address_zipcode', 'address_city']),
-          'phone': '+33' + _.get(order, 'phone', '0').slice(1),
-          'box_number': _.get(order, 'content[0].box.number', 'null'),
-          'date': date
-        }
-      )
+      const date = getDateFormatted(_.get(order, "created_at", ""));
+      csvColissimoOrders[box.number].push({
+        ..._.pick(order, [
+          "id",
+          "last_name",
+          "first_name",
+          "address",
+          "address_more",
+          "address_zipcode",
+          "address_city",
+        ]),
+        phone: "+33" + _.get(order, "phone", "0").slice(1),
+        box_number: _.get(order, "content[0].box.number", "null"),
+        date: date,
+      });
     }
-  })
+  });
 
-  let csvColissimoPaths = []
+  let csvColissimoPaths = [];
 
   for (const [number, boxOrders] of Object.entries(csvColissimoOrders)) {
     if (boxOrders.length > 0) {
-      const path = dirpath + 'orders_collisimo_box_' + number + '_' + today_7AM.getTime() + '.csv'
+      const path =
+        dirpath +
+        "orders_collisimo_box_" +
+        number +
+        "_" +
+        today_7AM.getTime() +
+        ".csv";
       const csvColissimoWriter = createCsvWriter({
         path: path,
         header: [
-          {id: 'date', title: 'date'},
-          {id: 'id', title: 'id'},
-          {id: 'last_name', title: 'nom'},
-          {id: 'first_name', title: 'prenom'},
-          {id: 'box_number', title: 'numéro boite'},
-          {id: 'address', title: 'adresse'},
-          {id: 'address_more', title: 'comple'},
-          {id: 'address_zipcode', title: 'cp'},
-          {id: 'address_city', title: 'ville'},
-          {id: 'phone', title: 'tph'}
-        ]
+          { id: "date", title: "date" },
+          { id: "id", title: "id" },
+          { id: "last_name", title: "nom" },
+          { id: "first_name", title: "prenom" },
+          { id: "box_number", title: "numéro boite" },
+          { id: "address", title: "adresse" },
+          { id: "address_more", title: "comple" },
+          { id: "address_zipcode", title: "cp" },
+          { id: "address_city", title: "ville" },
+          { id: "phone", title: "tph" },
+        ],
       });
 
-
-      await csvColissimoWriter.writeRecords(boxOrders)
+      await csvColissimoWriter.writeRecords(boxOrders);
 
       csvColissimoPaths.push({
         number: number,
-        path: path
-      })
+        path: path,
+      });
     }
   }
 
-  strapi.log.info('CSV COLISSIMO GENERATED WITH PATHS : ', csvColissimoPaths.map(r => r.path).join(' / '))
+  strapi.log.info(
+    "CSV COLISSIMO GENERATED WITH PATHS : ",
+    csvColissimoPaths.map((r) => r.path).join(" / ")
+  );
 
-  return csvColissimoPaths
+  return csvColissimoPaths;
 };
 
-const createMondialRelayCsv = async (dirpath, today_7AM, orders, environnement_id) => {
-
+const createMondialRelayCsv = async (
+  dirpath,
+  today_7AM,
+  orders,
+  environnement_id
+) => {
   const box_numbers = _.uniq(
     orders.map((order) => {
-      var box_number = _.get(order.content, '[0].box.number', null)
-      if (box_number) return box_number
+      var box_number = _.get(order.content, "[0].box.number", null);
+      if (box_number) return box_number;
     })
-  )
+  );
 
-  let csvMondialRelayOrders = {}
+  let csvMondialRelayOrders = {};
 
-  box_numbers.forEach((number) => csvMondialRelayOrders[number] = [])
+  box_numbers.forEach((number) => (csvMondialRelayOrders[number] = []));
 
   orders.forEach((order) => {
-    const box = _.get(order, 'content[0].box', {})
+    const box = _.get(order, "content[0].box", {});
     if (box.environnement === environnement_id) {
-      const date = getDateFormatted(_.get(order, 'created_at', ''))
-      csvMondialRelayOrders[box.number].push(
-        {
-          ..._.pick(order, ['id']),
-          'box_number': _.get(order, 'content[0].box.number', 'null'),
-          'date': date,
-          'address_poi': _.get(order, 'poi_name') + ', ' + _.get(order, 'address') + ', ' + _.get(order, 'address_zipcode') + ' ' + _.get(order, 'address_city'),
-          'mr_pdf_link': strapi.config.get('server.url') + '/uploads/orders/mondial-relay/order_mondial_relay_' + order.id + '.pdf'
-        }
-      )
+      const date = getDateFormatted(_.get(order, "created_at", ""));
+      csvMondialRelayOrders[box.number].push({
+        ..._.pick(order, ["id"]),
+        box_number: _.get(order, "content[0].box.number", "null"),
+        date: date,
+        address_poi:
+          _.get(order, "poi_name") +
+          ", " +
+          _.get(order, "address") +
+          ", " +
+          _.get(order, "address_zipcode") +
+          " " +
+          _.get(order, "address_city"),
+        mr_pdf_link:
+          strapi.config.get("server.url") +
+          "/uploads/orders/mondial-relay/order_mondial_relay_" +
+          order.id +
+          ".pdf",
+      });
     }
-  })
+  });
 
-  let csvMondialRelayPaths = []
+  let csvMondialRelayPaths = [];
 
   for (const [number, boxOrders] of Object.entries(csvMondialRelayOrders)) {
     if (boxOrders.length > 0) {
-      const path = dirpath + 'orders_mondial_relay_box_' + number + '_' + today_7AM.getTime() + '.csv'
+      const path =
+        dirpath +
+        "orders_mondial_relay_box_" +
+        number +
+        "_" +
+        today_7AM.getTime() +
+        ".csv";
       const csvMondialRelayWriter = createCsvWriter({
         path: path,
         header: [
-          {id: 'date', title: 'Date'},
-          {id: 'id', title: 'ID'},
-          {id: 'box_number', title: 'Boîte'},
-          {id: 'address_poi', title: 'Adresse du point relais'},
-          {id: 'mr_pdf_link', title: 'Lien vers étiquette'}
-        ]
+          { id: "date", title: "Date" },
+          { id: "id", title: "ID" },
+          { id: "box_number", title: "Boîte" },
+          { id: "address_poi", title: "Adresse du point relais" },
+          { id: "mr_pdf_link", title: "Lien vers étiquette" },
+        ],
       });
 
-
       if (boxOrders.length > 0) {
-        await csvMondialRelayWriter.writeRecords(boxOrders)
+        await csvMondialRelayWriter.writeRecords(boxOrders);
 
         csvMondialRelayPaths.push({
           number: number,
-          path: path
-        })
+          path: path,
+        });
       }
     }
   }
 
-  strapi.log.info('CSV MONDIAL RELAY GENERATED WITH PATHS : ', csvMondialRelayPaths.map(r => r.path).join(' / '))
+  strapi.log.info(
+    "CSV MONDIAL RELAY GENERATED WITH PATHS : ",
+    csvMondialRelayPaths.map((r) => r.path).join(" / ")
+  );
 
-  return csvMondialRelayPaths
+  return csvMondialRelayPaths;
 };
 
 module.exports = {
@@ -173,98 +220,164 @@ module.exports = {
    * Send email of daily orders
    * Every day at 7am.
    */
-  '0 5 * * *': async () => {
-    const environnements = await strapi.services.environnement.find()
-    const environnement_metropole = environnements.find(_ => _.slug === 'metropole')
-    const environnement_guyane = environnements.find(_ => _.slug === 'guyane')
+  "0 5 * * *": async () => {
+    const environnements = await strapi.services.environnement.find();
+    const environnement_metropole = environnements.find(
+      (_) => _.slug === "metropole"
+    );
+    const environnement_guyane = environnements.find(
+      (_) => _.slug === "guyane"
+    );
 
-    let yesterday_7AM  = new Date();
+    let yesterday_7AM = new Date();
     yesterday_7AM.setDate(yesterday_7AM.getDate() - 1);
     yesterday_7AM.setHours(5);
     yesterday_7AM.setMinutes(0);
     yesterday_7AM.setMilliseconds(0);
 
-    let today_7AM  = new Date();
+    let today_7AM = new Date();
     today_7AM.setHours(5);
     today_7AM.setMinutes(0);
     today_7AM.setMilliseconds(0);
 
-    const yesterdayFormatted = String(yesterday_7AM.getDate()).padStart(2, '0') + '/' + String(yesterday_7AM.getMonth() + 1).padStart(2, '0') + '/' + yesterday_7AM.getFullYear()
-    const todayFormatted = String(today_7AM.getDate()).padStart(2, '0') + '/' + String(today_7AM.getMonth() + 1).padStart(2, '0') + '/' + today_7AM.getFullYear()
+    const yesterdayFormatted =
+      String(yesterday_7AM.getDate()).padStart(2, "0") +
+      "/" +
+      String(yesterday_7AM.getMonth() + 1).padStart(2, "0") +
+      "/" +
+      yesterday_7AM.getFullYear();
+    const todayFormatted =
+      String(today_7AM.getDate()).padStart(2, "0") +
+      "/" +
+      String(today_7AM.getMonth() + 1).padStart(2, "0") +
+      "/" +
+      today_7AM.getFullYear();
 
-    const ordersColissimo = await strapi.services.commande.find({created_at_gte: yesterday_7AM.getTime(), created_at_lt: today_7AM.getTime(), delivery: 'home', sent: false, _sort: 'created_at:desc'})
-    const ordersMondialRelay = await strapi.services.commande.find({created_at_gte: yesterday_7AM.getTime(), created_at_lt: today_7AM.getTime(), delivery: 'pickup', sent: false, _sort: 'created_at:desc'})
+    const ordersColissimo = await strapi.services.commande.find({
+      created_at_gte: yesterday_7AM.getTime(),
+      created_at_lt: today_7AM.getTime(),
+      delivery: "home",
+      sent: false,
+      _sort: "created_at:desc",
+    });
+    const ordersMondialRelay = await strapi.services.commande.find({
+      created_at_gte: yesterday_7AM.getTime(),
+      created_at_lt: today_7AM.getTime(),
+      delivery: "pickup",
+      sent: false,
+      _sort: "created_at:desc",
+    });
 
-    const dirpath = 'public/uploads/orders/csv'
-    await fs.mkdirSync(dirpath, { recursive: true })
+    const dirpath = "public/uploads/orders/csv";
+    await fs.mkdirSync(dirpath, { recursive: true });
 
-    const csvColissimoPaths = await createColissimoCsv(dirpath, today_7AM, ordersColissimo, environnement_metropole.id)
-    const csvMondialRelayPaths = await createMondialRelayCsv(dirpath, today_7AM, ordersMondialRelay, environnement_metropole.id)
+    const csvColissimoPaths = await createColissimoCsv(
+      dirpath,
+      today_7AM,
+      ordersColissimo,
+      environnement_metropole.id
+    );
+    const csvMondialRelayPaths = await createMondialRelayCsv(
+      dirpath,
+      today_7AM,
+      ordersMondialRelay,
+      environnement_metropole.id
+    );
 
-    const attachments = []
+    const attachments = [];
     csvColissimoPaths.forEach((csvColissimoPath) => {
-      attachments.push(
-        {filename: 'commandes_colissimo_box_' + csvColissimoPath.number + '_' + yesterdayFormatted.split('/').join('') + '_' + todayFormatted.split('/').join('') + '.csv', content: fs.createReadStream(csvColissimoPath.path)}
-      )
-    })
+      attachments.push({
+        filename:
+          "commandes_colissimo_box_" +
+          csvColissimoPath.number +
+          "_" +
+          yesterdayFormatted.split("/").join("") +
+          "_" +
+          todayFormatted.split("/").join("") +
+          ".csv",
+        content: fs.createReadStream(csvColissimoPath.path),
+      });
+    });
     csvMondialRelayPaths.forEach((csvMondialRelayPath) => {
-      attachments.push(
-        {filename: 'commandes_mondial_relay_box_' + csvMondialRelayPath.number + '_' + yesterdayFormatted.split('/').join('') + '_' + todayFormatted.split('/').join('') + '.csv', content: fs.createReadStream(csvMondialRelayPath.path)}
-      )
-    })
+      attachments.push({
+        filename:
+          "commandes_mondial_relay_box_" +
+          csvMondialRelayPath.number +
+          "_" +
+          yesterdayFormatted.split("/").join("") +
+          "_" +
+          todayFormatted.split("/").join("") +
+          ".csv",
+        content: fs.createReadStream(csvMondialRelayPath.path),
+      });
+    });
 
-    await strapi.plugins['email'].services.email.sendTemplatedEmail(
+    await strapi.plugins["email"].services.email.sendTemplatedEmail(
       {
-        to: strapi.config.get('server.emails.cat'),
-        cc: strapi.config.get('server.emails.admin'),
-        attachments: attachments
+        to: strapi.config.get("server.emails.cat"),
+        cc: strapi.config.get("server.emails.admin"),
+        attachments: attachments,
       },
       EMAIL_DAILY_ORDERS,
       {
         yesterday_date: yesterdayFormatted,
-        today_date: todayFormatted
+        today_date: todayFormatted,
       }
-    )
+    );
 
-    strapi.log.info('EMAIL SENT / CRON OVER at ', new Date().toISOString())
+    strapi.log.info("EMAIL SENT / CRON OVER at ", new Date().toISOString());
   },
-  '0 5 * * *': async () => {
+  "0 5 * * *": async () => {
+    let yesterday_7AM = new Date();
+    yesterday_7AM.setDate(yesterday_7AM.getDate() - 1);
+    yesterday_7AM.setHours(5);
+    yesterday_7AM.setMinutes(0);
+    yesterday_7AM.setMilliseconds(0);
 
-    let yesterday_7AM  = new Date();
-      yesterday_7AM.setDate(yesterday_7AM.getDate() - 1);
-      yesterday_7AM.setHours(5);
-      yesterday_7AM.setMinutes(0);
-      yesterday_7AM.setMilliseconds(0);
+    let today_7AM = new Date();
+    today_7AM.setHours(5);
+    today_7AM.setMinutes(0);
+    today_7AM.setMilliseconds(0);
 
-    let today_7AM  = new Date();
-      today_7AM.setHours(5);
-      today_7AM.setMinutes(0);
-      today_7AM.setMilliseconds(0);
-
-    const cat_users = await strapi.query('user', 'users-permissions').find({'role.type': 'cat'})
-    const ordersColissimo = await strapi.services.commande.find({created_at_gte: yesterday_7AM.getTime(), created_at_lt: today_7AM.getTime(), delivery: 'home', sent: false, _sort: 'created_at:desc'})
-    const ordersMondialRelay = await strapi.services.commande.find({created_at_gte: yesterday_7AM.getTime(), created_at_lt: today_7AM.getTime(), delivery: 'pickup', sent: false, _sort: 'created_at:desc'})
+    const cat_users = await strapi
+      .query("user", "users-permissions")
+      .find({ "role.type": "cat" });
+    const ordersColissimo = await strapi.services.commande.find({
+      created_at_gte: yesterday_7AM.getTime(),
+      created_at_lt: today_7AM.getTime(),
+      delivery: "home",
+      sent: false,
+      _sort: "created_at:desc",
+    });
+    const ordersMondialRelay = await strapi.services.commande.find({
+      created_at_gte: yesterday_7AM.getTime(),
+      created_at_lt: today_7AM.getTime(),
+      delivery: "pickup",
+      sent: false,
+      _sort: "created_at:desc",
+    });
 
     await cat_users.forEach((user) => {
       const CAT_TEMPLATE = {
-      subject: `Tumeplay: ${ordersColissimo.length + ordersMondialRelay.length} nouvelle(s) commande(s) prête(s) à être traitées`,
-      text: `Bienvenue, pour rappel vous pouvez maintenant traiter les commandes ici :
+        subject: `Tumeplay: ${
+          ordersColissimo.length + ordersMondialRelay.length
+        } nouvelle(s) commande(s) prête(s) à être traitées`,
+        text: `Bienvenue, pour rappel vous pouvez maintenant traiter les commandes ici :
         https://bo-tumeplay.fabrique.social.gouv.fr/orders/box/1
         Pour toutes informations complémentaires merci de contacter l'équipe Tumeplay
       `,
-      html: `
+        html: `
             <p>Bienvenue, pour rappel vous pouvez maintenant traiter les commandes ici :
         https://bo-tumeplay.fabrique.social.gouv.fr/orders/box/1
         Pour toutes informations complémentaires merci de contacter l'équipe Tumeplay </p>
-      `
-      }
-      strapi.plugins['email'].services.email.sendTemplatedEmail(
+      `,
+      };
+      strapi.plugins["email"].services.email.sendTemplatedEmail(
         {
-          to: user.email
+          to: user.email,
         },
         CAT_TEMPLATE
-      )
-    })
-  }
-
+      );
+    });
+  },
 };
