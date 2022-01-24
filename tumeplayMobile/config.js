0bf17769193bb6e26e726fe7f0d3860558257f51
Frédéric S <85285770+FredericNumericite@users.noreply.github.com>
2022-01-24 12:09:34 +0100

    fix: responsive (#270)

---
+++ tumeplayMobile/config.js
@@ -0,0 +1,7 @@
+import {Dimensions} from 'react-native';
+const config = {
+  deviceWidth: Dimensions.get('window').width,
+  deviceHeight: Dimensions.get('window').height,
+};
+
+export default config;
