a503f5b65b4f4ff244dbd375d62ea666eca39b45
Clément <clement@numericite.eu>
2022-02-03 14:07:09 +0100

    feat: update strapi model & create python script to adapt data

--- back-strapi/api/question/controllers/question.js
+++ back-strapi/api/question/controllers/question.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { sanitizeEntity } = require('strapi-utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
@@ -7,26 +9,36 @@
 
 module.exports = {
 	async updateForMobileApp(ctx) {
-		const userFromContext = ctx.state.user; 
 		const body = ctx.request.body;
 
-		const question = await strapi.services['question'].findOne({ title: body.title });
-		if (!content) {
+		const question = await strapi.services['question'].findOne({ text_question: body.title });
+		if (!question) {
 			return ctx.badRequest(null, 'Question [' + body.title + '] does not exist');
 		}
 
-		let updatedContent = {
-			title_mobile: question.title,
-			kind: question.kind,
-			
+		let content_id
+		if (body.title_related_content && body.title_related_content !== "") {
+			const content = await strapi.services['content'].findOne({ title: body.title_related_content });
+			content_id = content.id || null
 		}
 
-		// if (body.new_title) {
-		// 	updatedContent.title_mobile = body.new_title
-		// }
+		let updatedQuestion = {	
+			text_question_mobile: body.newTitle || question.text_question,
+			kind: body.kind,
+			related_content: content_id,
+			responses: {
+				response_A_mobile: body.response_A_mobile || question.responses.response_A,
+				response_B_mobile: body.response_B_mobile || question.responses.response_B,
+				response_C_mobile: body.response_C_mobile || question.responses.response_C,
+			}
+		}
 
-		// const entity = await strapi.services['content'].update({id: content.id}, updatedContent)
+		if (body.new_title) {
+			updatedQuestion.text_question_mobile = body.new_title
+		}
 
-		// return sanitizeEntity(entity, { model: strapi.models.content });
+		const entity = await strapi.services['question'].update({id: question.id}, updatedQuestion)
+
+		return sanitizeEntity(entity, { model: strapi.models.question });
 	}
 };
--- data-update/updates.py
+++ data-update/updates.py
@@ -6,5 +6,10 @@ headers = {'Content-Type': 'application/json'}
 
 
 def updateContent(data):
-    r = requests.post('http://localhost:1337/contents/mobile-update', data=data)
+    r = requests.post('https://backend-tumeplay-preprod.dev.fabrique.social.gouv.fr/contents/mobile-update', data=data)
+    print(r.json())
+
+
+def updateQuestion(data):
+    r = requests.post('https://backend-tumeplay-preprod.dev.fabrique.social.gouv.fr/questions/mobile-update', data=data)
     print(r.json())
