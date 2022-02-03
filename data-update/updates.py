a3c800c86ce832d9bd1e40c2bd84f1e0c574a457
Clément <clement@numericite.eu>
2022-02-03 14:33:19 +0100

    feat: add title to mudole

--- data-update/updates.py
+++ data-update/updates.py
@@ -7,9 +7,19 @@ headers = {'Content-Type': 'application/json'}
 
 def updateContent(data):
     r = requests.post('https://backend-tumeplay-preprod.dev.fabrique.social.gouv.fr/contents/mobile-update', data=data)
-    print(r.json())
+    response = r.json()
+    try:
+        if response['statusCode'] == 400:
+            print(response['message'])
+    except KeyError:
+        pass
 
 
 def updateQuestion(data):
     r = requests.post('https://backend-tumeplay-preprod.dev.fabrique.social.gouv.fr/questions/mobile-update', data=data)
-    print(r.json())
+    response = r.json()
+    try:
+        if response['statusCode'] == 400:
+            print(response['message'])
+    except KeyError:
+        pass
