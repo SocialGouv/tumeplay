010667cb7d25ae3613d3e3f5847ba1222a943f67
Antoine Lelong <azenoxe@gmail.com>
2022-02-04 12:51:49 +0100

    fix: user not found (#285)
    * fix: add useLazyQuery to getUserMobile and in backend return user object and status 404 when user not found
    
    * fix: clean up console.log
    
    * fix: remove package-lock.json from gitignore
    
    * fix: rerender yarn.lock
    
    Co-authored-by: Clément <clement@numericite.eu>

--- tumeplayMobile/src/hooks/withApollo.js
+++ tumeplayMobile/src/hooks/withApollo.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import {ApolloProvider} from '@apollo/client/react';
-import {ApolloClient, InMemoryCache} from '@apollo/client';
+import {ApolloClient, InMemoryCache, HttpLink, from} from '@apollo/client';
 import {REACT_APP_API_URL} from '@env';
+import {onError} from '@apollo/client/link/error';
 
 const withAppolo = Component => props => {
-  const client = new ApolloClient({
-    uri: REACT_APP_API_URL,
-    cache: new InMemoryCache(),
+  const errorLink = onError(({networkError, graphQLErrors}) => {
+    // console.log('NetworkCode : ' + JSON.stringify(networkError, null, 2));
+    // console.log('GraphqlError : ' + JSON.stringify(graphQLErrors, null, 2));
   });
-
+  const httpLink = new HttpLink({
+    uri: REACT_APP_API_URL,
+  });
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: from([errorLink, httpLink]),
+  });
   return (
     <ApolloProvider client={client}>
       <Component {...props} />
