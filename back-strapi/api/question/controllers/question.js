74152509583e634e7c21ab01abb9b5f1b1e66965
Clément <clement@numericite.eu>
2022-02-04 15:22:11 +0100

    fix: avoid override responses

--- back-strapi/api/question/controllers/question.js
+++ back-strapi/api/question/controllers/question.js
@@ -27,6 +27,13 @@ module.exports = {
 			kind: body.kind,
 			related_content: content_id,
 			responses: {
+				response_A: question.responses.response_A,
+				response_B: question.responses.response_B,
+				response_C: question.responses.response_C,
+				response_A_neutral: question.responses.response_A_neutral,
+				response_B_neutral: question.responses.response_B_neutral,
+				response_C_neutral: question.responses.response_C_neutral,
+				right_answer: question.responses.right_answer,
 				response_A_mobile: body.response_A_mobile || question.responses.response_A,
 				response_B_mobile: body.response_B_mobile || question.responses.response_B,
 				response_C_mobile: body.response_C_mobile || question.responses.response_C,
