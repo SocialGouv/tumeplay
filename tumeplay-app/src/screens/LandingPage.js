5985890a7771711930b629eba6142b1fbde91165
Clément Lelong <clement@numericite.eu>
2022-02-21 12:19:46 +0100

    feat: add landing redirection for metropole (#336)
    * feat: add landing redirection for metropole
    
    * fix: title design
    
    * feat: redirect only android users to landing page
    
    * fix: only android and right link
    
    * fix: remove target blank (sonar issue)
    
    * fix: add alt to images landing

--- tumeplay-app/src/screens/LandingPage.js
+++ tumeplay-app/src/screens/LandingPage.js
@@ -1,67 +1,93 @@
-import React from 'react'
-import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native'
-import Colors from '../styles/Color'
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import Colors from '../styles/Color';
 
 const REACT_APP_ZONE = process.env.REACT_APP_ZONE;
 const REACT_APP_OTHER_ZONE_URL = process.env.REACT_APP_OTHER_ZONE_URL;
 
-const LandingPage = (props) => {
+const LandingPage = props => {
+  const france = require('../assets/pictures/cartefrance.svg');
+  const guyane = require('../assets/pictures/carteguyane.svg');
+  const logo = require('../assets/pictures/logoTumeplay.svg');
+  const param = '?zone_choice=true';
 
-  const france = require('../assets/pictures/cartefrance.svg')
-  const guyane = require('../assets/pictures/carteguyane.svg')
-  const logo = require('../assets/pictures/logoTumeplay.svg')
-  const param = "?zone_choice=true"
-
-  const handleRedirection = (name) => {
-    if(REACT_APP_ZONE === name) {
-      props.navigation.navigate('LandingScreen')
+  const handleRedirection = name => {
+		console.log(navigator.userAgent)
+    if (REACT_APP_ZONE === name) {
+      if (name === 'metropole' && navigator.userAgent.match(/Android/i)) {
+        window.location.href = 'landing.html';
+      } else {
+        props.navigation.navigate('LandingScreen');
+      }
     } else {
-      window.location.href = REACT_APP_OTHER_ZONE_URL + param
+      if (name === 'metropole') {
+        window.location.href = REACT_APP_OTHER_ZONE_URL + 'landing.html';
+      } else {
+        window.location.href = REACT_APP_OTHER_ZONE_URL + param;
+      }
     }
-  }
+  };
 
-  return(
+  return (
     <View style={style.container}>
-      <Image style={style.logo} source={logo} resizeMode='contain'/>
+      <Image style={style.logo} source={logo} resizeMode="contain" />
       <View style={style.titleContainer}>
         <Text style={style.title}>Hello ! Où es-tu ?</Text>
       </View>
       <View style={style.cardContainer}>
         <View style={style.column}>
-          <TouchableOpacity style={style.card} onPress={() => {handleRedirection('metropole')}}>
-            <Image style={style.image} resizeMode='contain' source={france}/>
+          <TouchableOpacity
+            style={style.card}
+            onPress={() => {
+              handleRedirection('metropole');
+            }}>
+            <Image style={style.image} resizeMode="contain" source={france} />
           </TouchableOpacity>
-          <TouchableOpacity style={style.button} >
-            <Text style={style.text} onPress={() => {handleRedirection('metropole')}}>Hexagone</Text>
+          <TouchableOpacity style={style.button}>
+            <Text
+              style={style.text}
+              onPress={() => {
+                handleRedirection('metropole');
+              }}>
+              Hexagone
+            </Text>
           </TouchableOpacity>
         </View>
         <View style={style.column}>
-          <TouchableOpacity style={style.card} onPress={() => {handleRedirection('guyane')}}>
-            <Image style={style.image} resizeMode='contain' source={guyane}/>
+          <TouchableOpacity
+            style={style.card}
+            onPress={() => {
+              handleRedirection('guyane');
+            }}>
+            <Image style={style.image} resizeMode="contain" source={guyane} />
           </TouchableOpacity>
-          <TouchableOpacity style={style.button} onPress={() => {handleRedirection('guyane')}}>
+          <TouchableOpacity
+            style={style.button}
+            onPress={() => {
+              handleRedirection('guyane');
+            }}>
             <Text style={style.text}>Guyane</Text>
           </TouchableOpacity>
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const style = StyleSheet.create({
   container: {
     flex: 1,
     height: 100,
     backgroundColor: Colors.backgroundColor,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   logo: {
     width: 250,
     height: 250,
     justifyContent: 'center',
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
-  appTitle:{
+  appTitle: {
     color: Colors.secondaryText,
     fontFamily: Colors.appTitleFont,
     fontSize: 44,
@@ -90,7 +116,7 @@ const style = StyleSheet.create({
   },
   column: {
     flexDirection: 'column',
-    width: "50%",
+    width: '50%',
   },
   card: {
     flex: 1,
@@ -98,7 +124,7 @@ const style = StyleSheet.create({
     borderRadius: 15,
   },
   text: {
-    color: "#FFFF",
+    color: '#FFFF',
     fontFamily: Colors.appTitleFont,
     fontSize: 20,
     marginVertical: 10,
@@ -108,18 +134,18 @@ const style = StyleSheet.create({
     width: 200,
     height: 200,
     position: 'absolute',
-    top: "50%",
-    left: "50%",
+    top: '50%',
+    left: '50%',
     transform: 'translate(-50%, -50%)',
     alignSelf: 'center',
-		cursor: 'pointer',
-    opacity: 1
+    cursor: 'pointer',
+    opacity: 1,
   },
   button: {
     backgroundColor: Colors.mainButton,
     borderRadius: 35,
     marginHorizontal: 20,
-  }
-})
+  },
+});
 
 export default LandingPage;
