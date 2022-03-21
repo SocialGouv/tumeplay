f025bcf3e723a220c509cb18eb5371736ea54cde
BrianRid <brianridolce@gmail.com>
2022-03-21 19:01:09 +0100

    feat: creating specific file for redirection logic

--- tumeplayMobile/src/components/Navbar.js
+++ tumeplayMobile/src/components/Navbar.js
@@ -11,57 +11,12 @@ import QuizzLoader from './global/QuizzLoader';
 import diceIcon from '../assets/diceIcon.png';
 import {Image} from 'react-native';
 import Event from '../services/api/matomo';
+import handleRedirection from '../services/handleRedirection';
 
 const Tab = createBottomTabNavigator();
 
 const Navbar = ({navigation}) => {
   const {user} = useContext(AppContext);
-
-  const handleQuizzRedirection = () => {
-    if (user.pending_module) {
-      return {
-        module_id: user.pending_module.id,
-        module_title: user.pending_module.title,
-        theme: {
-          title: user.pending_module.theme_title,
-          image: user.pending_module.theme_image,
-          color: user.pending_module.theme_color,
-        },
-        questions: user.pending_module_questions,
-        homeScreen: true,
-        clearModuleData: true,
-        retry: false,
-      };
-    } else if (user.next_module) {
-      return {
-        module_id: user.next_module.id,
-        module_title: user.next_module.title,
-        theme: {
-          title: user.next_module.theme_title,
-          image: user.next_module.theme_image,
-          color: user.next_module.theme_color,
-        },
-        questions: user.next_module_questions,
-        homeScreen: true,
-        clearModuleData: true,
-        retry: false,
-      };
-    } else {
-      return {
-        module_id: user.random_module.id,
-        module_title: user.random_module.title,
-        questions: user.random_module_questions,
-        theme: {
-          title: user.random_module.theme_title,
-          image: user.random_module.theme_image,
-          color: user.random_module.theme_color,
-        },
-        homeScreen: true,
-        clearModuleData: true,
-        retry: false,
-      };
-    }
-  };
 
   return (
     <Tab.Navigator
@@ -118,7 +73,7 @@ const Navbar = ({navigation}) => {
             Event.playEvent('navbar');
           },
         }}
-        initialParams={handleQuizzRedirection()}
+        initialParams={handleRedirection(user)}
       />
       <Tab.Screen
         name="Parcours"
--- tumeplayMobile/src/views/HomePage.js
+++ tumeplayMobile/src/views/HomePage.js
@@ -14,6 +14,7 @@ import Carousel from 'react-native-snap-carousel';
 import config from '../../config';
 import {WebView} from 'react-native-webview';
 import Event from '../services/api/matomo';
+import handleRedirection from '../services/handleRedirection';
 
 const HomePage = ({navigation}) => {
   const {user} = useContext(AppContext);
@@ -119,52 +120,6 @@ const HomePage = ({navigation}) => {
     });
   };
 
-  const handleQuizzRedirection = () => {
-    if (user.pending_module) {
-      return {
-        module_id: user.pending_module.id,
-        module_title: user.pending_module.title,
-        theme: {
-          title: user.pending_module.theme_title,
-          image: user.pending_module.theme_image,
-          color: user.pending_module.theme_color,
-        },
-        questions: user.pending_module_questions,
-        homeScreen: true,
-        clearModuleData: true,
-        retry: false,
-      };
-    } else if (user.next_module) {
-      return {
-        module_id: user.next_module.id,
-        module_title: user.next_module.title,
-        theme: {
-          title: user.next_module.theme_title,
-          image: user.next_module.theme_image,
-          color: user.next_module.theme_color,
-        },
-        questions: user.next_module_questions,
-        homeScreen: true,
-        clearModuleData: true,
-        retry: false,
-      };
-    } else {
-      return {
-        module_id: user.random_module.id,
-        module_title: user.random_module.title,
-        questions: user.random_module_questions,
-        theme: {
-          title: user.random_module.theme_title,
-          image: user.random_module.theme_image,
-          color: user.random_module.theme_color,
-        },
-        homeScreen: true,
-        clearModuleData: true,
-        retry: false,
-      };
-    }
-  };
-
   return (
     <ScrollView>
       <Container background={null} style={styles.container}>
@@ -184,7 +139,7 @@ const HomePage = ({navigation}) => {
             left
             onPress={() => {
               Event.playEvent('home');
-              navigation.navigate('Jouer', handleQuizzRedirection());
+              navigation.navigate('Jouer', handleRedirection(user));
             }}
             icon
           />
