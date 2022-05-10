f5ab9ef2519e9d811e1dac35c689037106214a22
Adrien Chauve <adrien@chauve.eu>
2022-05-10 17:04:22 +0200

    feat(ci): use kube-workflow

--- .k8s/utils/getImageTag.ts
+++
@@ -1,7 +0,0 @@
-export default (env: Record<string, any>) => {
-  const ref =
-    env.GITHUB_REF && env.GITHUB_REF.startsWith("refs/tags/")
-      ? env.GITHUB_REF.split("/").pop().replace(/^v/, "")
-      : `sha-${env.GITHUB_SHA}`;
-  return ref;
-};
