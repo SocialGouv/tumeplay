2b1f9bfa687f6a88a91c77773bada6a2ad1b2a81
BrianRid <51997673+BrianRid@users.noreply.github.com>
2022-07-22 11:36:51 +0200

    feat: adding precision field on other response selection in referent … (#419)
    * feat: adding precision field on other response selection in referent intention measuremen
    
    * feat: referent intention css positionning

--- tumeplayMobile/src/services/api/referents.js
+++ tumeplayMobile/src/services/api/referents.js
@@ -12,6 +12,7 @@ export const CREATE_REFERENT_INTENTION = gql`
   mutation createSurveyReferent(
     $is_interested: Boolean
     $detailed_informations: String
+    $other_informations: String
     $utilisateurs_mobile: ID
   ) {
     createReferentSurvey(
@@ -19,6 +20,7 @@ export const CREATE_REFERENT_INTENTION = gql`
         data: {
           is_interested: $is_interested
           detailed_informations: $detailed_informations
+          other_informations: $other_informations
           utilisateurs_mobile: $utilisateurs_mobile
         }
       }
@@ -29,5 +31,3 @@ export const CREATE_REFERENT_INTENTION = gql`
     }
   }
 `;
-
-
