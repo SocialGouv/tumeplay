bddd84cf54503b4bf40af065e33d0cacd3642365
Clément <clement@numericite.eu>
2022-09-21 14:10:53 +0200

    feat: add dockerfile && matomo config

--- tumeplay-vitrine/pages/_app.tsx
+++ tumeplay-vitrine/pages/_app.tsx
@@ -5,9 +5,18 @@ import type { AppProps } from "next/app";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { extendedTheme } from "../utils/chakra-theme";
 
+import { init } from "@socialgouv/matomo-next";
+import { useEffect } from "react";
+
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL as string;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID as string;
 const theme = extendTheme(extendedTheme);
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
