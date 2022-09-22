a39b7e0a022ffd233dfb4eeec4c66046ad323fa2
Clément <clement@numericite.eu>
2022-09-22 10:45:53 +0200

    fix: push forgotten component

---
+++ tumeplay-vitrine/components/BackButton.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@chakra-ui/react";
+import { BackButtonProps } from "./interfaces";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const BackButton = ({ text }: BackButtonProps) => {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  if (router.events) {
+    router.events.on("routeChangeStart", () => {
+      setLoading(true);
+    });
+
+    router.events.on("routeChangeComplete", () => {
+      setLoading(false);
+    });
+  }
+
+  return (
+    <Button
+      onClick={() => {
+        router.back();
+      }}
+      rounded="xl"
+      borderColor="#dadada"
+      borderWidth={1}
+      isLoading={loading}
+    >
+      <ArrowBackIcon mr={1} />
+      {text || "Retour"}
+    </Button>
+  );
+};
+
+export default BackButton;
