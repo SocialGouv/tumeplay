a9ca88e6ebf99477cd2fa14a094813170aa7e769
BrianRid <51997673+BrianRid@users.noreply.github.com>
2022-11-02 14:42:03 +0100

    feat: adding links to open mandatory websites (#457)
    * feat: adding links to open mandatory websites
    
    * feat: code smell optimization

---
+++ tumeplayMobile/src/components/global/LinkOpenner.js
@@ -0,0 +1,30 @@
+import {TouchableOpacity, Linking, View} from 'react-native';
+import React from 'react';
+
+const LinkOpenner = ({children, url}) => {
+  const onPress = () => {
+    Linking.canOpenURL(url).then(supported => {
+      if (supported) {
+        Linking.openURL(url);
+      } else {
+        console.log("Don't know how to open URI: " + url);
+      }
+    });
+  };
+
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          width: '100%',
+          marginVertical: 10,
+        }}>
+        {children}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default LinkOpenner;
