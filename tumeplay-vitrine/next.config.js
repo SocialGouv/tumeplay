42e7985addb9eb594190b2329d11475266c5b625
Clément <clement@numericite.eu>
2022-11-04 19:51:13 +0100

    fix: next config remove i18n

--- tumeplay-vitrine/next.config.js
+++ tumeplay-vitrine/next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n: {
-    locales: ["fr"],
-    defaultLocale: "fr",
-  },
   images: {
     domains: [
       "backend-tumeplay.fabrique.social.",
