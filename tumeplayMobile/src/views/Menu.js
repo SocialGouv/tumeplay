47256430ebc553531640822ca9762c0e2526c48a
Frédéric S <85285770+FredericNumericite@users.noreply.github.com>
2023-07-18 13:25:24 +0200

    feat: add sos button in menu page (#473)
    * feat: add sos button in menu page
    
    * feat: remove call permission in manifest + update handleCall function
    
    * feat: center text
    
    * feat: add matomo tracker
    
    * feat: update phone number
    
    * feat: update phone number in description
    
    * fix: ios bundle for build"
    
    ---------
    
    Co-authored-by: BrianRid <brianridolce@gmail.com>

--- tumeplayMobile/src/views/Menu.js
+++ tumeplayMobile/src/views/Menu.js
@@ -1,4 +1,4 @@
-import {StyleSheet, View, TouchableOpacity} from 'react-native';
+import {StyleSheet, View, TouchableOpacity, Linking} from 'react-native';
 import Container from '../components/global/Container';
 import React from 'react';
 import Text from '../components/Text';
@@ -7,9 +7,22 @@ import SponsorCard from '../components/Menu/SponsorCard';
 import config from '../../config';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import LinkOpenner from '../components/global/LinkOpenner';
+import Event from '../services/api/matomo';
 
 const Menu = () => {
   const navigation = useNavigation();
+  const handleCall = () => {
+    const phoneNumber = '0800235236';
+    const dialNumber = `tel:${phoneNumber}`;
+
+    Linking.openURL(dialNumber).catch(error => {
+      console.log(
+        "Erreur lors de l'ouverture du composeur téléphonique:",
+        error,
+      );
+    });
+    Event.sosButtonClickEvent();
+  };
   return (
     <Container style={styles.container}>
       <TouchableOpacity
@@ -38,6 +51,19 @@ const Menu = () => {
           <Icon name="open-in-new" size={15} style={styles.icon} />
           <Text>Conditions Générales d'Utilisation</Text>
         </LinkOpenner>
+        <View style={styles.sosContainer}>
+          <TouchableOpacity style={styles.sosButton} onPress={handleCall}>
+            <Icon color="#ffffff" name="call" size={24} />
+            <Text style={styles.sosContent}>SOS Sexualité</Text>
+          </TouchableOpacity>
+          <Text style={styles.sosTitle}>
+            Sexualité / Contraception / IVG - 0 800 235 236 :
+          </Text>
+          <Text style={styles.sosDescription}>
+            Numéro gratuit pour répondre à toutes les questions sur les
+            sexualités, la contraception et l’IVG.
+          </Text>
+        </View>
       </View>
     </Container>
   );
@@ -65,6 +91,34 @@ const styles = StyleSheet.create({
   },
   icon: {
     marginRight: 10,
+  },
+  sosContainer: {
+    marginTop: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  sosButton: {
+    flexDirection: 'row',
+    backgroundColor: '#000000',
+    borderRadius: 16,
+    alignItems: 'center',
+    padding: 10,
+    marginBottom: 10,
+    justifyContent: 'center',
+    color: 'white',
+    width: '50%',
+  },
+  sosContent: {
+    color: 'white',
+    paddingLeft: 10,
+    fontWeight: '600',
+  },
+  sosTitle: {
+    fontWeight: '600',
+  },
+  sosDescription: {
+    textAlign: 'center',
   },
 });
 
