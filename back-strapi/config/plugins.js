6503e746c984a40711d76a78059588aad548ac6e
FredericNumericite <frederic@numericite.eu>
2023-11-14 16:27:31 +0100

    fix: update smtp conf

--- back-strapi/config/plugins.js
+++ back-strapi/config/plugins.js
@@ -1,19 +1,21 @@
 module.exports = ({ env }) => ({
-    email: {
-      provider: 'smtp',
+  email: {
+    config: {
+      provider: "smtp",
       providerOptions: {
-        host: env('EMAIL_PROVIDER_HOST'), //SMTP Host
-        port: env('EMAIL_PROVIDER_PORT'), //SMTP Port
-        secure: env('EMAIL_PROVIDER_SECURE') === 'true',
-        username: env('EMAIL_PROVIDER_USERNAME'),
-        password: env('EMAIL_PROVIDER_PASSWORD'),
+        host: env("EMAIL_PROVIDER_HOST"), //SMTP Host
+        port: env("EMAIL_PROVIDER_PORT"), //SMTP Port
+        secure: env("EMAIL_PROVIDER_SECURE") === "true",
+        username: env("EMAIL_PROVIDER_USERNAME"),
+        password: env("EMAIL_PROVIDER_PASSWORD"),
         rejectUnauthorized: true,
-        requireTLS: env('EMAIL_PROVIDER_TLS') === 'true',
+        requireTLS: env("EMAIL_PROVIDER_TLS") === "true",
         connectionTimeout: 1,
       },
       settings: {
-        from: env('EMAIL_PROVIDER_FROM'),
-        replyTo: env('EMAIL_PROVIDER_REPLY_TO'),
+        from: env("EMAIL_PROVIDER_FROM"),
+        replyTo: env("EMAIL_PROVIDER_REPLY_TO"),
       },
     },
-  });
+  },
+});
