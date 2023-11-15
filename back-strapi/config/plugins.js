e0ba5e442cf2abdfbacf43aa14e3be8e92205a14
FredericNumericite <frederic@numericite.eu>
2023-11-15 11:26:57 +0100

    feat: add tipimail smtp

--- back-strapi/config/plugins.js
+++ back-strapi/config/plugins.js
@@ -1,21 +1,18 @@
 module.exports = ({ env }) => ({
   email: {
-    config: {
-      provider: "smtp",
-      providerOptions: {
-        host: env("EMAIL_PROVIDER_HOST"), //SMTP Host
-        port: env("EMAIL_PROVIDER_PORT"), //SMTP Port
-        secure: env("EMAIL_PROVIDER_SECURE") === "true",
-        username: env("EMAIL_PROVIDER_USERNAME"),
-        password: env("EMAIL_PROVIDER_PASSWORD"),
-        rejectUnauthorized: true,
-        requireTLS: env("EMAIL_PROVIDER_TLS") === "true",
-        connectionTimeout: 1,
-      },
-      settings: {
-        from: env("EMAIL_PROVIDER_FROM"),
-        replyTo: env("EMAIL_PROVIDER_REPLY_TO"),
-      },
+    provider: "smtp",
+    providerOptions: {
+      host: env("EMAIL_PROVIDER_HOST"),
+      port: env("EMAIL_PROVIDER_PORT"),
+      username: env("EMAIL_PROVIDER_USERNAME"),
+      password: env("EMAIL_PROVIDER_PASSWORD"),
+      rejectUnauthorized: true,
+      requireTLS: "true",
+      connectionTimeout: 1,
+    },
+    settings: {
+      defaultFrom: env("EMAIL_PROVIDER_FROM"),
+      defaultReplyTo: env("EMAIL_PROVIDER_REPLY_TO"),
     },
   },
 });
